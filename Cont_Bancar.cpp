0574c447f3c9b4d5eed92b60468ea4e1fd226c50
Cristea Razvan-Andrei <razvy2001@yahoo.com>
2021-08-02 19:48:58 +0300

    Add project files.

---
+++ Cont_Bancar.cpp
@@ -0,0 +1,10 @@
+#include "Cont_Bancar.h"
+
+Cont_Bancar::Cont_Bancar(std::string nume, std::string prenume, std::string IBAN)
+{
+	this->nume = nume; // this este un pointer care anuleaza ambiguitatea intre un camp al unei clase si 
+	this->prenume = prenume;//parametrul unui constructor care sunt de acelasi tip si denumire
+	this->IBAN = IBAN;
+	sold = 0;
+	e_tip_cont = Tip_cont::CONT_RON;
+}
---
+++ Cont_Bancar.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+enum class Tip_cont // am creat o enumeratie care are specificatorul de acces pe public (default)!!!!
+{
+	CONT_RON,
+	CONT_EURO,
+	CONT_DOLAR
+};
+
+class Cont_Bancar
+{
+	std::string nume;
+	std::string prenume;
+	int sold;
+	Tip_cont e_tip_cont;
+	std::string IBAN;
+
+public:
+	Cont_Bancar(std::string nume, std::string prenume, std::string IBAN);
+	~Cont_Bancar();
+
+};
+
